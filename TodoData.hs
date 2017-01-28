17fef36055d13d638f91a5445a3529571764db9f
Kevin Moonen <dragonrider7225@gmail.com>
2017-01-28 15:04:10 -0600

    Added parsers for tasks

--- TodoData.hs
+++ TodoData.hs
@@ -11,16 +11,41 @@ data Days = Days { sun :: Bool
                  }
 
 instance Show Days where
-    show d = map (head . show . fromEnum . ($ d)) [sat,fri,thu,wed,tue,mon,sun]
+    show d = map (head . show . fromEnum . ($ d)) [sun,mon,tue,wed,thu,fri,sat]
+
+data Task = Task { name :: String
+                 , repNum :: Int
+                 , reps :: Days
+                 }
+
+instance Show Task where
+    show t = (name t) ++ " (" ++ (show $ repNum t) ++ ") " ++ (show $ reps t)
+
+taskP :: Parser Task
+taskP = do
+    name <- taskNameP
+    num <- repNumP
+    reps <- space >> daysP
+    return $ Task name num reps
+
+taskNameP :: Parser String
+taskNameP = do
+    nameSp <- many $ noneOf "("
+    return $ init nameSp
+
+repNumP :: Parser Int
+repNumP = do
+    char '('
+    numStr <- many (oneOf "0123456789")
+    char ')'
+    return (read numStr :: Int)
 
 -- A Days object consists of seven digits, either '0' or '1'.
-days :: Parser Days
-days = do
+daysP :: Parser Days
+daysP = do
     daysStr <- sequence . take 7 . repeat $ char '0' <|> char '1'
     let [su, mo, tu, we, th, fr, sa] = map (toEnum . read . (:[]) :: Char -> Bool) daysStr
     return $ Days su mo tu we th fr sa
 
-data Task = Task { name :: String
-                 , repNum :: Maybe Int
-                 , reps :: Maybe Days
-                 } deriving (Show)
+testParser :: String -> Either ParseError Task
+testParser = parse taskP "(unknown)"
